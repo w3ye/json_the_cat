43dea2a11443393a156d54404c17fe1f9985aa3f
William Ye <w3ye@pm.me>
2021-07-29 16:53:51 -0400

    Alter the api according the arg recieved

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,10 @@
 const request = require('request');
-const api = 'https://api.thecatapi.com/v1/breeds/search?q=siberian';
 const name = process.argv[2];
+const api = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
 
 request(api, (err, response, body) => {
   if (err) return console.log(`Error: Incorrect url ${api}`);
   let bodyJson = JSON.parse(body);
   if (bodyJson.length === 0) return console.log("Cat breed not found");
-  
+  console.log(bodyJson);
 });
